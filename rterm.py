7eb16168105730ed4fdae140b4b2f3c27f981260
chriswang2468 <xinchao@ualberta.ca>
2015-11-12 23:47:07 -0700

    pterm donegit statusgit statusgit statusgit status!

--- rterm.py
+++ rterm.py
@@ -5,12 +5,13 @@ import replaceFile
 import re
 fileName = input("please enter your file name ")
 replaceFile.replaceFile(fileName)
-tempFile=open('pterms.txt','w+')
+tempFile=open('rterms.txt','w+')
 blist=[]
 clist=[]
 alist=replaceFile.makeList(fileName)
 for i, a in enumerate(alist):
-    c = a[1].split()
+    c = a[8].split()
+    c1=a[9].split()
     if c[0][0]=="\"" or c[-1][-1]=="\"":
         #line = "".join(line.rsplit(k))
         tep = c[0]
@@ -19,7 +20,15 @@ for i, a in enumerate(alist):
         tep1 = c[-1]
         tep1 = tep1[:-1]
         c[-1] = tep1
-    blist.append(c)
+    if c1[0][0]=="\"" or c1[-1][-1]=="\"":
+        #line = "".join(line.rsplit(k))
+        tep = c1[0]
+        tep = tep[1:]
+        c1[0] = tep
+        tep1 = c1[-1]
+        tep1 = tep1[:-1]
+        c1[-1] = tep1
+    blist.append(c+c1)
 for b in blist:
     b=" ".join(b)
     b=re.sub('\W+',' ',b)
