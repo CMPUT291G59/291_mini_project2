30c014ca527f5b8cd8243c8bd101b06349b3ccc4
chriswang2468 <xinchao@ualberta.ca>
2015-11-13 19:53:20 -0700

    linux command in python

---
+++ sortFile.py
@@ -0,0 +1,14 @@
+from subprocess import Popen, PIPE
+
+p = Popen('sort -u -o pterms.txt pterms.txt', shell=True,
+          stdout=PIPE, stderr=PIPE)
+out, err = p.communicate()
+p1 = Popen('sort -u -o rterms.txt rterms.txt', shell=True,
+          stdout=PIPE, stderr=PIPE)
+out, err = p1.communicate()
+p2 = Popen('sort -u -o scores.txt scores.txt', shell=True,
+          stdout=PIPE, stderr=PIPE)
+out, err = p2.communicate()
+
+
+
